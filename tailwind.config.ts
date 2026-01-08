3e8f369323cc2a39c95d8290e53e9453e4bdc24f
Luiz Paulo Moreno Ragi <lpmragi@gmail.com>
2026-01-07 21:07:15 -0300

    teste

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,17 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+        serif: ['var(--font-playfair)'],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
   },
   plugins: [],
 };
