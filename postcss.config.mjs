98b7024a2eed20864079e49bec36434ac850bd59
Luiz Paulo Moreno Ragi <lpmragi@gmail.com>
2026-01-07 21:15:53 -0300

    Fix: Corrigindo sintaxe CSS do antialiased

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {}, // <--- AQUI MUDOU (Antes era só 'tailwindcss')
+    '@tailwindcss/postcss': {}, // <--- AQUI MUDOU: Usando o pacote novo da v4
     autoprefixer: {},
   },
 };
