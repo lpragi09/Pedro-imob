04f69f056e7b9eeab5c6c9701fdb4a392a00dba4
Luiz Paulo Moreno Ragi <lpmragi@gmail.com>
2026-01-07 21:13:45 -0300

    Corrigindo dependencia do Tailwind v4

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // <--- AQUI MUDOU (Antes era só 'tailwindcss')
     autoprefixer: {},
   },
 };
